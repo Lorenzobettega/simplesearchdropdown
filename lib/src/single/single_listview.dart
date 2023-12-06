7deb95e9196c5f619167be6bc67f849367fe01e5
Lorenzobettega <lore.bettega@gmail.com>
2023-12-06 13:42:48 -0300

    missing single drop list view size correction

--- lib/src/single/single_listview.dart
+++ lib/src/single/single_listview.dart
@@ -78,56 +78,60 @@ class SingleListView<T> extends StatelessWidget {
         updateShouldScroll();
       }
     });
-    return Card(
-      surfaceTintColor:
-          overlayListSettings.dialogBackgroundColor ?? backgroundColor,
-      color: overlayListSettings.dialogBackgroundColor ?? backgroundColor,
-      elevation: elevation,
-      child: AnimatedContainer(
-        duration: overlayListSettings.animationDuration,
-        height: overlayListSettings.dialogHeight,
-        width: dropdownwidth,
-        child: ListView.separated(
-          controller: controller,
-          padding: EdgeInsets.zero,
-          itemCount: listaFiltrada.length + (addMode ? 1 : 0),
-          separatorBuilder: (context, index) => SizedBox(
-            height: overlayListSettings.separatorHeight,
-          ),
-          itemBuilder: (context, index) {
-            if (index == listaFiltrada.length && addMode) {
-              if (searchbarText != '') {
-                final list = listaFiltrada
-                    .where(
-                      (element) =>
-                          element.label.toLowerCase().latinize().contains(
-                                searchbarText.toLowerCase().latinize(),
-                              ),
-                    )
-                    .toList();
-                if (list.isEmpty) {
-                  return DefaultAddListItem(
-                    itemAdded: itemAdded,
+    return Stack(
+      children: [
+        Card(
+          surfaceTintColor:
+              overlayListSettings.dialogBackgroundColor ?? backgroundColor,
+          color: overlayListSettings.dialogBackgroundColor ?? backgroundColor,
+          elevation: elevation,
+          child: AnimatedContainer(
+            duration: overlayListSettings.animationDuration,
+            height: overlayListSettings.dialogHeight,
+            width: dropdownwidth,
+            child: ListView.separated(
+              controller: controller,
+              padding: EdgeInsets.zero,
+              itemCount: listaFiltrada.length + (addMode ? 1 : 0),
+              separatorBuilder: (context, index) => SizedBox(
+                height: overlayListSettings.separatorHeight,
+              ),
+              itemBuilder: (context, index) {
+                if (index == listaFiltrada.length && addMode) {
+                  if (searchbarText != '') {
+                    final list = listaFiltrada
+                        .where(
+                          (element) =>
+                              element.label.toLowerCase().latinize().contains(
+                                    searchbarText.toLowerCase().latinize(),
+                                  ),
+                        )
+                        .toList();
+                    if (list.isEmpty) {
+                      return DefaultAddListItem(
+                        itemAdded: itemAdded,
+                        overlayListSettings: overlayListSettings,
+                        text: searchbarText,
+                      );
+                    }
+                  }
+                  return const SizedBox.shrink();
+                } else {
+                  return DefaultListTile<T>(
+                    deleteMode: deleteMode,
+                    item: listaFiltrada[index],
+                    onDelete: onDelete,
+                    onPressed: onPressed,
                     overlayListSettings: overlayListSettings,
-                    text: searchbarText,
+                    selected: searchbarText == listaFiltrada[index].label,
+                    key: index == 0 ? _itemKey : null,
                   );
                 }
-              }
-              return const SizedBox.shrink();
-            } else {
-              return DefaultListTile<T>(
-                deleteMode: deleteMode,
-                item: listaFiltrada[index],
-                onDelete: onDelete,
-                onPressed: onPressed,
-                overlayListSettings: overlayListSettings,
-                selected: searchbarText == listaFiltrada[index].label,
-                key: index == 0 ? _itemKey : null,
-              );
-            }
-          },
+              },
+            ),
+          ),
         ),
-      ),
+      ],
     );
   }
 }
